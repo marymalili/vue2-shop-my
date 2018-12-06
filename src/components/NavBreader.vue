b7132a4b72ea4f09aed2f0636a568dd4d611421f
mali <li.ma@wrinatec.com>
2018-12-06 15:59:34 +0800

    GoodsList组件拆分，修正了HelloWorld

---
+++ src/components/NavBreader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="nav-breadcrumb-wrap">
+      <div class="container">
+        <nav class="nav-breadcrumb">
+          <a href="/">主页</a>
+          <span>商品列表</span>
+        </nav>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "NavBreader",
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
